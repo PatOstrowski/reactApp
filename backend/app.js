e7b18398a1f5788c3249200c6846dc898a7151b2
Patryk Ostrowski <ostrowski.pat@gmail.com>
2026-06-04 12:28:31 +0200

    Dodanie CORS do backendu

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const createError = require("http-errors");
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
@@ -14,6 +15,7 @@ const dealsRouter = require("./routes/deals");
 const tipsRouter = require("./routes/tips");
 
 const app = express();
+app.use(cors());
 
 // Database setup
 mongoose
